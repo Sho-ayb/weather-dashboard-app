adaae85ac37622e4a835a0ed5d9339dace5d42ff
Sho.ayb <Sho.ayb@outlook.com>
2023-01-29 11:24:23 +0000

    main html and css styles partially completed

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// lets start with wrapping all our code within a jquery wrapper for the document: ensures that the script will only execute when the document is ready
+
+$(document).ready(function () {
+  console.log("Document has loaded");
+});
