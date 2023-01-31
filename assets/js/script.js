29eefb76b109475bd3a5a14260fa5b105820a68b
Sho.ayb <Sho.ayb@outlook.com>
2023-01-31 10:36:38 +0000

    forecasts are now being rendered to application

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,18 @@
 $(document).ready(function () {
   console.log("Document has loaded");
 
-  // lets use moment to format the date they way we want it
+  // lets use moment to format the date
 
   const now = moment();
 
   const todaysDate = now.format("dddd, MMMM D");
 
-  $(".date-text").text(todaysDate);
-
   // lets put todays date in to element
+  $(".date-text").text(todaysDate);
 
   // lets create global variables here
 
   const apiKey = "d0af7ceac9a3501bc47a8577610395a2";
-
-  // lets create a function to listen to the form event
-
-  // lets query select all the elements we need to present the weather forecast
 
   // we need the element for the unordered list of todays forecast
 
@@ -29,7 +24,7 @@ $(document).ready(function () {
 
   const weatherListFourDay = $(".weather-list-fourday");
 
-  // lets query select the weather item and remove it
+  // lets query select the weather item and remove it from the DOM, when the page loads
 
   const weatherItem = $(".weather-item");
 
@@ -103,7 +98,7 @@ $(document).ready(function () {
               console.log("Retrieved weather data successfully: ", data);
               // lets pass the data object to a function
               displayTodayWeather(data);
-              //   displayFourDayWeather(data);
+              displayForecast(data);
             });
           })
           .catch((error) => {
@@ -111,6 +106,10 @@ $(document).ready(function () {
 
             if (error.status === 404) {
               $(".form-message").text("Please enter a valid City name ! 😮");
+            }
+
+            if (error.status === 400) {
+              $(".form-message").text("Please enter a City name ! 😮");
             }
           });
 
@@ -122,6 +121,8 @@ $(document).ready(function () {
       // lets create a separate function here to display the todays forecast in to correct section
 
       const displayTodayWeather = (data) => {
+        $(".weather-list-today").empty();
+
         // lets extract the weather list array
 
         let weatherList = data.list;
@@ -131,12 +132,6 @@ $(document).ready(function () {
         // lets destructure the object props from the above array
 
         const { main, weather, wind } = weatherList[0];
-
-        const listItemsArr = Array.from(
-          weatherListToday.querySelectorAll(".weather-item")
-        );
-
-        console.log(listItemsArr);
 
         // lets setup the list item markup on the todays forecast
 
@@ -148,7 +143,7 @@ $(document).ready(function () {
           <li class="weather-item">
             <h2 class="days">Today</h2>
             <h3 class="date">
-                <span>${todaysDate}</span>
+                <span>${moment(weatherList[0].dt_txt).format("D MMMM")}</span>
             </h3>
             <h2 class="city-name" data-city-name="${data.city.name.toLowerCase()}">
             <span>${data.city.name}</span>
@@ -173,30 +168,71 @@ $(document).ready(function () {
 
         // lets create a list item
 
-        const li = todaysMarkup;
+        li = todaysMarkup;
 
-        // and append it to the unordered list here
+        // and append it to the unordered list
 
         weatherListToday.append(li);
-
-        // $(".weather-today").append(weatherListToday);
       };
 
-      //   const displayFourDayWeather = (data) => {
-      //     // now that we have the five day array forecast, we need to build a list item markup
-      //     // by loop through this array - starting the array @ 1 because we only need the other 4 objects to complete the five day forecast
+      const displayForecast = (data) => {
+        $(".weather-list-fourday").empty();
+        // now that we have the five day array forecast, we need to build a list item markup
+        // by loop through this array - starting the array @ 1 because we only need the other 4 objects to complete the five day forecast
 
-      //     for (let i = 1; i < fiveDayArr.length; i++) {
-      //       // lets first destructure the object props we need from the array of objects
-      //       const { main, weather, wind } = fiveDayArr[i];
+        const weatherList = data.list;
 
-      //       console.log("main ", main, "weather ", weather, "wind ", wind);
-      //     }
-      //   };
+        console.log(weatherList);
+
+        for (let i = 5; i < weatherList.length; i = i + 10) {
+          // lets first destructure the object props we need from the array of objects
+          const { main, weather, wind } = weatherList[i];
+
+          console.log("main ", main, "weather ", weather, "wind ", wind);
+
+          const forecastMarkup = `
+            
+              
+          <li class="weather-item">
+            <h2 class="days">${moment(weatherList[i].dt_txt).format(
+              "dddd"
+            )}</h2>
+            <h3 class="date">
+                <span>${moment(weatherList[i].dt_txt).format("D MMMM")}</span>
+            </h3>
+            <h2 class="city-name" data-city-name="${data.city.name.toLowerCase()}">
+            <span>${data.city.name}</span>
+            <sup>${data.city.country}</sup>
+          </h2>
+          <figure>
+            <img class="city-icon" src="http://openweathermap.org/img/wn/${
+              weather[0].icon
+            }@4x.png" alt="${weather[0].description}" />
+            <figcaption>${weather[0].description}</figcaption>
+          </figure>
+          <h3 class="weather-data">
+            <span class="city-temp">Temp: ${Math.round(
+              main.temp
+            )}<sup>&#176;C</sup></span>
+            <span class="city-humid">Humid: ${main.humidity}</span>
+            <span class="city-wind">Wind: ${Math.round(wind.speed)}</span>
+            </h3>
+        </li>
+        
+        `;
+
+          // lets create the list items
+
+          const li = forecastMarkup;
+
+          // and append it to the unordered list
+
+          weatherListFourDay.append(li);
+        }
+      };
 
       // lets invoke functions here in main()
       getLatLng();
-      displayTodayWeather();
     });
   };
 
